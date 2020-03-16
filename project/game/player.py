83cc1da6913d9f4e42c84202924139a303f05961
ohste <ohste@oregonstate.edu>
2020-03-16 16:08:30 -0700

    updated player.py checking

--- project/game/player.py
+++ project/game/player.py
@@ -1,5 +1,6 @@
 # Ben-Ryder 2019
-
+"""Defines player and enemies actions and movements
+including control and path seeking."""
 import random
 import pygame
 
@@ -9,110 +10,131 @@ import paths
 from project import game
 
 
-# Encompasses the key interface for all moving entities (ie player, enemies..). To be inherited, not used directly.
-# (Based of Player class taken from here: https://www.pygame.org/project-Rect+Collision+Response-1061-.html)
+
+# This should be inherited, not used directly.
+# (Based of Player class taken from here:
+# https://www.pygame.org/project-Rect+Collision+Response-1061-.html)
+
 class BasePlayer:
+    """Encompasses the key interface for all moving entities (ie player, enemies..)."""
     def __init__(self, position, start_vector, level):
         self.vector = start_vector
         self.linked_level = level
         self.rect = pygame.Rect(position[0], position[1], level.PLAYER_SIZE, level.PLAYER_SIZE)
-        # default rect only to prevent IDE highlighting. SHOULD BE OVERWRITTEN IN SUBCLASS INIT where assets are loaded!
+        # default rect only to prevent IDE highlighting.
+        #SHOULD BE OVERWRITTEN IN SUBCLASS INIT where assets are loaded!
 
     def update(self):
+        """Update player position."""
         self.move()
 
-    def move_by(self, dx, dy):
-        # Move each axis separately. Note that this checks for collisions both times.
-        if dx != 0:
-            self.move_single_axis(dx, 0)
-        if dy != 0:
-            self.move_single_axis(0, dy)
+    def move_by(self, x_change, y_change):
+        """Move each axis separately. Note that this checks for collisions both times."""
+        if x_change != 0:
+            self.move_single_axis(x_change, 0)
+        if y_change != 0:
+            self.move_single_axis(0, y_change)
 
     def move(self):
+        """Move 1 in the y direction."""
         self.move_by(self.vector[0], self.vector[1])
 
     def stop_horizontal(self):
+        """Stop horizontal velocity."""
         self.vector[0] = 0
 
     def stop_vertical(self):
+        """Stop veritcal velocity."""
         self.vector[1] = 0
 
-    def move_single_axis(self, dx, dy):
-        self.rect.x += dx
-        self.rect.y += dy
-
+    def move_single_axis(self, x_change, y_change):
+        """Move the player checking for wall collsions first."""
+        self.rect.x += x_change
+        self.rect.y += y_change
         # If you collide with a wall, move out based on velocity
         for wall in self.linked_level.get_walls():
             if self.rect.colliderect(wall.rect):
-                if dx > 0:  # Moving right; Hit the left side of the wall
+                if x_change > 0:  # Moving right; Hit the left side of the wall
                     self.rect.right = wall.rect.left
                     self.vector[0] = 0
-                if dx < 0:  # Moving left; Hit the right side of the wall
+                if x_change < 0:  # Moving left; Hit the right side of the wall
                     self.rect.left = wall.rect.right
                     self.vector[0] = 0
-                if dy > 0:  # Moving down; Hit the top side of the wall
+                if y_change > 0:  # Moving down; Hit the top side of the wall
                     self.rect.bottom = wall.rect.top
                     self.vector[1] = 0
-                if dy < 0:  # Moving up; Hit the bottom side of the wall
+                if y_change < 0:  # Moving up; Hit the bottom side of the wall
                     self.rect.top = wall.rect.bottom
                     self.vector[1] = 0
 
 
 # Custom Players (using specific sprite assets, so has certain hardcoded values + files)
-
 class UserPLayer(BasePlayer):
+    """Defines a custom player that uses hardcoded values and files."""
     def __init__(self, position, start_vector, level):
         super().__init__(position, start_vector, level)
         self.speed = 3
-        
+
         # Sprite Image Setup
         self.image = pygame.image.load(paths.imagePath + "player.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, [level.PLAYER_SIZE, level.PLAYER_SIZE])
         self.rect = self.image.get_rect().move(position)
 
     def move_up(self):
+        """Move up based on speed."""
         self.move_by(0, -self.speed)
 
     def move_down(self):
+        """Move down based on speed."""
         self.move_by(0, self.speed)
 
     def move_left(self):
+        """Move left based on speed."""
         self.move_by(-self.speed, 0)
 
     def move_right(self):
+        """Move right based on speed."""
         self.move_by(self.speed, 0)
 
     def draw(self, display):
+        """Put the drawn image in square."""
         display.blit(self.image, [self.rect.x, self.rect.y])
 
 
 class Timer:  # starts tracking from point of declaration.
+    """Timer to check that game is still in progress."""
     def __init__(self, sec, mins=0, hrs=0):
         self.target = sec + mins*60 + hrs*60*60
         self.start = pygame.time.get_ticks()
 
     def has_ended(self):
+        """Checks if game has ended."""
         sec = (pygame.time.get_ticks()-self.start)/1000
         return sec >= self.target
 
 
 class PatrolTimer(Timer):  # starts tracking from point of declaration.
+    """Used as timer for EnemyPatrol."""
     def __init__(self, sec, mins=0, hrs=0):
         super().__init__(sec, mins, hrs)
         self.active = False
 
     def is_active(self):
+        """Is the timer active."""
         return self.active
 
-    def set_new(self, sec):  # only seconds needed for patrol, so interface hides mins and hrs.
+    def set_new(self, sec):
+        """Only seconds needed for patrol, so interface hides mins and hrs."""
         self.__init__(sec)
         self.active = True
 
     def end(self):
+        """Has patrol ended."""
         self.active = False
 
 
 class EnemyPatrol(BasePlayer):
+    """Defines the patrol enemy which follows certain route on a timer."""
     def __init__(self, level, patrol):
         # level is custom object, requiring .format for grid format and .walls for collision_objects
 
@@ -127,8 +149,9 @@ class EnemyPatrol(BasePlayer):
 
         self.patrol = patrol  # list of key points to visit
         self.current_target = 0  # index of patrol list
-        self.path = []    # list of all points between current patrol point and next point.
-        # path works as stack. index 0 is always next square target, pop off when reached for next target.
+        self.path = [] # list of all points between current patrol point and next point.
+        # path works as stack. index 0 is always next square target
+        # pop off when reached for next target.
         self.timer = PatrolTimer(0)
         # timer used to pause for so long on a square
 
@@ -149,8 +172,8 @@ class EnemyPatrol(BasePlayer):
 
         elif len(self.path) > 0:
             # Checking if reached next tile, if so stepping target to next in path.
-            x, y = game.get_pixel_position(self.linked_level, self.path[0])
-            self.target_rect = pygame.Rect(x, y, self.linked_level.TILE_SIZE, self.linked_level.TILE_SIZE)
+            x_pos, y_pos = game.get_pixel_position(self.linked_level, self.path[0])
+            self.target_rect = pygame.Rect(x_pos, y_pos, self.linked_level.TILE_SIZE, self.linked_level.TILE_SIZE)
             self.target_rect.inflate_ip(-self.linked_level.ENEMY_PADDING, -self.linked_level.ENEMY_PADDING)
 
             reached_target = False
@@ -165,43 +188,51 @@ class EnemyPatrol(BasePlayer):
 
             if reached_target:
                 self.step_path()
-                self.rect = self.target_rect  # assigning so player is re-centered to tile if gone > boundary.
+                # assigning so player is re-centered to tile if gone > boundary.
+                self.rect = self.target_rect
 
         else:
             self.step_patrol()
             self.update_path()
 
     def step_path(self):
+        """Pop the path from the stack."""
         self.path.pop(0)
         if len(self.path) > 0:
             self.update_vector()
 
     def update_vector(self):
+        """Update direction of player/enemy."""
         if self.timer.is_active():
             self.vector = [0, 0]
         else:
-            if game.get_tile_position(self.linked_level, self.rect.topleft)[0] < self.path[0][0]:
-                self.vector[0] = self.speed
-            elif game.get_tile_position(self.linked_level, self.rect.topleft)[0] > self.path[0][0]:
-                self.vector[0] = -self.speed
-            else:
-                self.stop_horizontal()
+            try:
+                if game.get_tile_position(self.linked_level, self.rect.topleft)[0] < self.path[0][0]:
+                    self.vector[0] = self.speed
+                elif game.get_tile_position(self.linked_level, self.rect.topleft)[0] > self.path[0][0]:
+                    self.vector[0] = -self.speed
+                else:
+                    self.stop_horizontal()
 
-            if game.get_tile_position(self.linked_level, self.rect.topleft)[1] < self.path[0][1]:
-                self.vector[1] = self.speed
-            elif game.get_tile_position(self.linked_level, self.rect.topleft)[1] > self.path[0][1]:
-                self.vector[1] = -self.speed
-            else:
-                self.stop_vertical()
+                if game.get_tile_position(self.linked_level, self.rect.topleft)[1] < self.path[0][1]:
+                    self.vector[1] = self.speed
+                elif game.get_tile_position(self.linked_level, self.rect.topleft)[1] > self.path[0][1]:
+                    self.vector[1] = -self.speed
+                else:
+                    self.stop_vertical()
+            except IndexError:
+                print(self, self.path)
 
     def step_patrol(self):
+        """Continue patrolling."""
         if self.current_target < len(self.patrol)-1:
             self.current_target += 1
         else:
             self.current_target = 0
 
     def update_path(self):
-        if type(self.patrol[self.current_target]) not in [int, float]:
+        """Create a path for the enemy."""
+        if not isinstance(self.patrol[self.current_target], (int, float)):
             self.path = game.GridPath(self.format,
                                       game.get_tile_position(self.linked_level, self.rect.topleft),
                                       self.patrol[self.current_target], constants.WALL_FORMATS).get_path()
@@ -212,6 +243,7 @@ class EnemyPatrol(BasePlayer):
         self.update_vector()
 
     def draw(self, display):
+        """Draw custom enemy."""
         display.blit(self.image, [self.rect.x, self.rect.y])
         # pygame.draw.rect(display, (100, 0, 0), self.rect, 1) # collision rect, for visual test only
 
@@ -231,6 +263,7 @@ class EnemyPatrol(BasePlayer):
 
 
 def get_random_patrol(level, spawn, points=10):
+    """Get random patrol."""
     random_patrol = [spawn]
     while len(random_patrol) < points:
         point = level.get_random_point()
@@ -240,11 +273,11 @@ def get_random_patrol(level, spawn, points=10):
             random_patrol.append(point)
             if random.randrange(1, 10) < 4:  # 4/10 chance to pause.
                 random_patrol.append(round(random.uniform(1, 3), 2))
-
     return random_patrol
 
 
 class RandomPatrol(EnemyPatrol):
+    """Defines the random patroller enemy."""
     def __init__(self, level, spawn, points=10):
         # level is custom object, requiring .format for grid format and .walls for collision_objects
         patrol = get_random_patrol(level, spawn, points)
@@ -271,16 +304,15 @@ class RandomPatrol(EnemyPatrol):
 
 
 class EnemySeeker(EnemyPatrol):
+    """Defines the seeker enemy."""
     def __init__(self, level, player, spawn):
         super().__init__(level, [spawn])
         self.target_player = player
-
         # Sprite Image Setup
         self.image = pygame.image.load(paths.imagePath + "enemy-seeker.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, [level.ENEMY_SIZE, level.ENEMY_SIZE])
         self.rect = self.image.get_rect().move(game.get_pixel_position(self.linked_level, spawn))
         self.rect.move_ip(level.ENEMY_PADDING/2, level.ENEMY_PADDING/2)
-
         self.active = False  # if it can chase after the player
 
     def update(self):
@@ -289,8 +321,8 @@ class EnemySeeker(EnemyPatrol):
         if self.active:
             if len(self.path) > 0:
                 # Checking if reached next tile, if so stepping target to next in path.
-                x, y = game.get_pixel_position(self.linked_level, self.path[0])
-                self.target_rect = pygame.Rect(x, y, self.linked_level.TILE_SIZE, self.linked_level.TILE_SIZE)
+                x_pos, y_pos = game.get_pixel_position(self.linked_level, self.path[0])
+                self.target_rect = pygame.Rect(x_pos, y_pos, self.linked_level.TILE_SIZE, self.linked_level.TILE_SIZE)
                 self.target_rect.inflate_ip(-self.linked_level.ENEMY_PADDING, -self.linked_level.ENEMY_PADDING)
 
                 reached_target = False
@@ -305,7 +337,9 @@ class EnemySeeker(EnemyPatrol):
 
                 if reached_target:
                     self.step_path()
-                    self.rect = self.target_rect  # assigning so player is re-centered to tile if gone > boundary.
+
+                    # assigning so player is re-centered to tile if gone > boundary.
+                    self.rect = self.target_rect
                     self.update_path()
 
             else:
---
+++ project/game/test.py
@@ -0,0 +1,10 @@
+import random
+import pygame
+
+import constants
+import paths
+
+from project import game
+
+level = project.game.level.Level
+spawn = [21,9]
